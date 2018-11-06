7a6186ffd4a864f8d012a21dfaef5370c7536eee
ochobitsagencia <custommarketinfo@gmail.com>
2018-11-06 14:18:37 -0300

    initial commit

---
+++ index.php
@@ -0,0 +1,363 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Clementine Clothing</title>
+    <!-- Google Font -->
+    <link href='http://fonts.googleapis.com/css?family=Dosis:300,400,500,600,700,800' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- Font Awesome -->
+
+
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+     <!-- Preloader -->
+    <link rel="stylesheet" href="css/preloader.css" type="text/css" media="screen, print"/>
+
+    <!-- Icon Font-->
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/owl.carousel.css">
+    <link rel="stylesheet" href="css/owl.theme.default.css">
+    <!-- Animate CSS-->
+    <link rel="stylesheet" href="css/animate.css">
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Style -->
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- Responsive CSS -->
+    <link href="css/responsive.css" rel="stylesheet">
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="js/lte-ie7.js"></script>
+	  <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+	  <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+                <!-- Preloader -->
+                <div id="preloader">
+                    <div id="status">&nbsp;</div>
+                </div>
+    <header id="HOME" style="background-position: 50% -125px;">
+	        <div class="section_overlay">
+	            <nav class="navbar navbar-default navbar-fixed-top" style="background-color:#f5d7cd">
+	              <div class="container">
+	                <!-- Brand and toggle get grouped for better mobile display -->
+	                <div class="navbar-header">
+	                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+	                    <span class="sr-only">Toggle navigation</span>
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                    <span class="icon-bar"></span>
+	                  </button>
+	                  <a class="navbar-brand" href="#"><img src="images/logo.png" alt=""></a>
+	                </div>
+
+	                <!-- Collect the nav links, forms, and other content for toggling -->
+	                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	                  <ul class="nav navbar-nav navbar-right">
+	                    <li><a href="#HOME">Inicio</a></li>
+	                    <li><a href="#ABOUT">Sobre Nosotros</a></li>
+	                    <li><a href="#WORK">Colecciones</a></li>
+	                    <li><a href="#CONTACT">Contacto</a></li>
+	                  </ul>
+	                </div><!-- /.navbar-collapse -->
+	              </div><!-- /.container -->
+	            </nav> 
+
+	            <div class="container">
+	                <div class="row">
+	                    <div class="col-md-12 text-center">
+	                        <div class="home_text wow fadeInUp animated">
+	                            <h2 style="font-size:30px">Clementine Clothing</h2>
+	                            <p></p>
+	                            <img src="images/shape.png" alt="">                        
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>  
+	    </section>         
+    </header>
+    <section class="about_us_area" id="ABOUT">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="about_title">
+                        <h2>Sobre Nosotros</h2>
+                        <img src="images/shape.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12  wow fadeInRight animated">
+                    <p class="about_us_p" style="font-size:17px">Clementine nace en el año 2011 con la idea de crear prendas &uacute;nicas tanto para el dia como para la noche, en donde se destaca la figura femenina combinando cortes simples y cl&aacute;sicos con inspiraci&oacute;n en los años '50 '60 y '70.</p>
+                </div>
+
+            <div class="col-md-12 wow fadeInRight animated" style="margin-top: 100px;">
+                <div class="container-fluid" >
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="single_image">
+                        <img src="images/i1.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="single_image">
+                        <img src="images/i2.jpg" alt="">               
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="single_image">
+                        <img src="images/i3.jpg" alt="">                     
+                    </div>
+                </div>
+                 <div class="col-md-3">
+                    <div class="single_image">
+                        <img src="images/i4.jpg" alt="">
+                                           
+                    </div>
+                </div>
+            </div>
+            <div class="row" style="padding-top:30px">
+               
+              
+                <div class="col-md-3">
+                    <div class="single_image">
+                        <img src="images/i6.jpg" alt="">
+                                            
+                    </div>
+                </div>
+                  <div class="col-md-3">
+                    <div class="single_image ">
+                        <img src="images/i7.jpg" alt="">
+                                            
+                    </div>
+                </div>
+                  <div class="col-md-3">
+                    <div class="single_image ">
+                        <img src="images/i8.jpg" alt="">
+                                            
+                    </div>
+                </div>
+                  <div class="col-md-3">
+                    <div class="single_image last_padding">
+                        <img src="images/i5.jpg" alt="">                   
+                    </div>
+                </div>
+            </div>           
+        </div>
+    </div>
+            </div>
+        </div>
+    </section>
+    
+   
+    <section class="work_area" id="WORK">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="work_title  wow fadeInUp animated">
+                        <h1>Colecciones</h1>
+                        <img src="images/shape.png" alt="">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-4 no_padding">
+                    <div class="single_image">
+                        <img src="images/w1.jpg" alt="">
+                        <div class="image_overlay">
+                            <h2>SS17</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 no_padding">
+                    <div class="single_image">
+                        <img src="images/w2.jpg" alt="">
+                        <div class="image_overlay">
+                            <h2>FW16</h2>
+                        </div>                        
+                    </div>
+                </div>
+                <div class="col-md-4 no_padding">
+                    <div class="single_image">
+                        <img src="images/w3.jpg" alt="">
+                        <div class="image_overlay">
+                            <h2>SS16</h2>
+                        </div>                        
+                    </div>
+                </div>
+            </div>
+            <div class="row pad_top">
+                <div class="col-md-4 no_padding">
+                    <div class="single_image">
+                        <img src="images/w5.jpg" alt="">
+                        <div class="image_overlay">
+                            <h2>FW15</h2>
+                        </div>                        
+                    </div>
+                </div>
+                <div class="col-md-4 no_padding">
+                    <div class="single_image">
+                        <img src="images/w4.jpg" alt="">
+                        <div class="image_overlay">
+                            <h2>SS15</h2>
+                        </div>                        
+                    </div>
+                </div>
+                <div class="col-md-4 no_padding">
+                    <div class="single_image last_padding">
+                        <img src="images/w6.jpg" alt="">
+                        <div class="image_overlay">
+                            <h2>FW14</h2>
+                        </div>                        
+                    </div>
+                </div>
+            </div>           
+        </div>
+    </section>
+
+   <div class="fun_facts">
+        <section class="header parallax home-parallax page" id="fun_facts" style="background-position: 50% -150px;">
+            <div class="section_overlay">
+                <div class="container">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="contact_title  wow fadeInUp animated">
+                        <h1 style="color:white">Contacto</h1>
+                        <img src="images/shape.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3  wow fadeInLeft animated">
+                    <div class="single_contact_info">
+                        <h2 style="color:#f73b56">Telefono</h2>
+                        <p style="color:white">+54 9 11 6017 3850</p>
+                    </div>
+                    <div class="single_contact_info">
+                        <h2 style="color:#f73b56">Email</h2>
+                        <p style="color:white">info@clothingclementine.com</p>
+                    </div>
+                    <div class="single_contact_info">
+                        <h2 style="color:#f73b56">Ciudad</h2>
+                        <p style="color:white">Lomas de Zamora, Bs. As.</p>
+                    </div>
+                </div>
+                <div class="col-md-9  wow fadeInRight animated">
+                        <?php
+                if (!isset($_POST['email'])) {
+                ?>
+                <form action="<?=$_SERVER['PHP_SELF']?>" method="post" class="contact-form">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" name="nombre" placeholder="Nombre">
+                                <input type="email" class="form-control" name="email" placeholder="Mail">
+                            </div>
+                            <div class="col-md-6">
+                                <textarea class="form-control" name="mensaje" rows="25" cols="10" placeholder="  Mensaje..."></textarea>
+                                <input type="submit" class="btn btn-default submit-btn form_submit" value="Enviar"/>                                
+                            </div>
+                        </div>
+                    </form>
+                    <?php
+}
+else{
+  $mensaje="Mensaje del formulario de contacto Clementine";
+  $mensaje.= "\nNombre: ". $_POST['nombre'];
+  $mensaje.= "\nEmail: ".$_POST['email'];
+  $mensaje.= "\nMensaje: \n".$_POST['mensaje'];
+  $destino= "info@clothingclementine.com";
+  $remitente = $_POST['email'];
+  $asunto = "Mensaje enviado por: ".$_POST['name'];
+  $mail = mail($destino,$asunto,$mensaje,"FROM: $remitente");
+ if ($mail)
+ {
+?>
+  <p><strong>Mensaje enviado.</strong></p>
+<?php
+}
+else
+{
+?>
+  <p><strong>Error al enviar mensaje.</strong></p>
+<?php
+}
+}
+?>
+
+                </div>
+            </div>
+        </div>
+                </div>            
+            </div>
+        </section>    
+    </div>
+<footer>
+    <div class="container">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="footer_logo   wow fadeInUp animated">
+                        <img src="images/logo.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center   wow fadeInUp animated">
+                    <div class="social">
+                        <h2>Seguinos</h2>
+                        <ul class="icon_list">
+                            <li><a href="http://www.facebook.com/clothingclementine" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                            <li><a href="http://www.instagram.com/clemclothing"target="_blank"><i class="fa fa-instagram"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <div class="copyright_text   wow fadeInUp animated">
+                        <p>Copyright © 2016 Clementine Clothing. | Design: <a href="http://www.ochobitsagencia.com" target="_blank">{8bits}</a></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- =========================
+     SCRIPTS 
+============================== -->
+
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/jquery.nicescroll.js"></script>
+    <script src="js/owl.carousel.js"></script>
+    <script src="js/wow.js"></script>
+    <script src="js/script.js"></script>
+
+</body>
+
+</html>
